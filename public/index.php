a48b3b809ba69e58179025451872a7d19222509a
Alongwe2 <alex_longwell@hotmail.com>
2018-02-01 11:48:26 -0500

    Push Test 2

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Path to your craft/ folder
-$craftPath = 'C:/wamp64/www/softwareIntegration/craft';
+$craftPath = 'C:/wamp64/www/softwareIntegration/craft';     
 
 // Do not edit below this line
 $path = rtrim($craftPath, '/').'/app/index.php';
